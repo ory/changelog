cd9bb1aeb5b8cc816d78c149ee5d31cf236ec9e9
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-04-17 23:36:21 +0200

    feat: adapt angular preset (#1)

---
+++ parser-opts.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  noteKeywords: [
+    'BREAKING CHANGE',
+    'BREAKING CHANGES',
+  ]
+}
---
+++ writer-opts.js
@@ -0,0 +1,134 @@
+'use strict'
+
+const compareFunc = require('compare-func')
+const Q = require('q')
+const readFile = Q.denodeify(require('fs').readFile)
+const resolve = require('path').resolve
+
+module.exports = Q.all([
+  readFile(resolve(__dirname, './templates/template.hbs'), 'utf-8'),
+  readFile(resolve(__dirname, './templates/header.hbs'), 'utf-8'),
+  readFile(resolve(__dirname, './templates/commit.hbs'), 'utf-8'),
+  readFile(resolve(__dirname, './templates/footer.hbs'), 'utf-8')
+])
+  .spread((template, header, commit, footer) => {
+    const writerOpts = getWriterOpts()
+
+    writerOpts.mainTemplate = template
+    writerOpts.headerPartial = header
+    writerOpts.commitPartial = commit
+    writerOpts.footerPartial = footer
+
+    return writerOpts
+  })
+
+var types = {
+  feat: 'Features',
+  fix: 'Bug Fixes',
+  perf: 'Performance Improvements',
+  refactor: 'Code Refactoring',
+  security: 'Security Improvements',
+  docs: 'Documentation',
+  style: 'Styles',
+  test: 'Tests',
+  build: 'Build System',
+  chore: 'Chores',
+  autogen: 'Code Generation',
+  ci: 'Continuous Integration',
+  revert: 'Reverts',
+  other: 'Unclassified'
+}
+
+var ignoreTypes = [
+  'style',
+  'chore',
+  'autogen',
+  'ci'
+]
+
+function getWriterOpts() {
+  return {
+    transform: (commit, context) => {
+      let discard = true
+      const issues = []
+
+      commit.notes.forEach(note => {
+        note.title = 'BREAKING CHANGES'
+        discard = false
+      })
+
+      if (commit.revert) {
+        commit.type = 'revert'
+      }
+
+      if (!types[commit.type]) {
+        commit.type = 'other'
+      }
+
+      if (ignoreTypes.indexOf(commit.type) > -1 && discard) {
+        return
+      }
+
+      commit.type = types[commit.type]
+
+      if (commit.scope === '*') {
+        commit.scope = ''
+      }
+
+      if (typeof commit.hash === 'string') {
+        commit.shortHash = commit.hash.substring(0, 7)
+      }
+
+      if (typeof commit.subject === 'string') {
+        let url = context.repository
+          ? `${context.host}/${context.owner}/${context.repository}`
+          : context.repoUrl
+        if (url) {
+          url = `${url}/issues/`
+          // Issue URLs.
+          commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+            issues.push(issue)
+            return `[#${issue}](${url}${issue})`
+          })
+        }
+        if (context.host) {
+          // User URLs.
+          commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
+            if (username.includes('/')) {
+              return `@${username}`
+            }
+
+            return `[@${username}](${context.host}/${username})`
+          })
+        }
+        commit.subject = commit.subject.charAt(0).toUpperCase() + commit.subject.substring(1);
+      }
+
+      // remove references that already appear in the subject
+      commit.references = commit.references.filter(reference => {
+        if (issues.indexOf(reference.issue) === -1) {
+          return true
+        }
+
+        return false
+      })
+
+      commit.hasBody = false
+      if (typeof commit.body === 'string') {
+        commit.body = commit.body.replace(/^signed-off-by: .*$/mi, '')
+        commit.body = commit.body.replace(/^co-authored-by: .*$/mi, '')
+        if (commit.body.trim().length > 0) {
+          commit.body = commit.body.split('\n').map((text)=>`  > ${text}`).join('\n')
+          commit.hasBody = true
+        }
+      }
+
+      return commit
+    },
+    groupBy: 'type',
+    commitGroupsSort: 'title',
+    commitsSort: ['type', 'scope', 'subject'],
+    noteGroupsSort: 'title',
+    notesSort: compareFunc
+  }
+}
