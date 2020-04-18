462789c45d841c5eb946041003730da70a5cdf4e
aeneasr <aeneas@ory.sh>
2020-04-18 11:05:44 +0200

    feat: add dedicated email preset

---
+++ email.js
@@ -0,0 +1,26 @@
+'use strict'
+const Q = require('q')
+const conventionalChangelog = require('./conventional-changelog')
+const parserOpts = require('./parser-opts')
+const recommendedBumpOpts = require('./conventional-recommended-bump')
+const writerOpts = require('./writer-opts')
+
+
+module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
+  .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
+   Q.all([
+      readFile(resolve(__dirname, './templates/email/template.hbs'), 'utf-8'),
+      readFile(resolve(__dirname, './templates/email/header.hbs'), 'utf-8'),
+      readFile(resolve(__dirname, './templates/email/commit.hbs'), 'utf-8'),
+      readFile(resolve(__dirname, './templates/email/footer.hbs'), 'utf-8')
+    ])
+      .spread((template, header, commit, footer) => {
+
+        writerOpts.mainTemplate = template
+        writerOpts.headerPartial = header
+        writerOpts.commitPartial = commit
+        writerOpts.footerPartial = footer
+
+        return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts }
+      })
+  })
